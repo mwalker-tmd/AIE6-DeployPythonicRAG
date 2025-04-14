4ae6c83706eb4ce8e42abf74a6b7238fa31f7599
Mark Walker <mwalker22@users.noreply.huggingface.co>
2025-04-14 01:22:35 +0000

    DEPLOYMENT: Investigating the cause of the Runtime Error.

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api import endpoints
 
+#TEMP: Looking for cause of Runtime Error.
+print("✅ FastAPI is booting...")
+print("PYTHONPATH:", os.getenv("PYTHONPATH"))
+
 app = FastAPI()
 
 # CORS middleware to allow frontend to talk to the backend
