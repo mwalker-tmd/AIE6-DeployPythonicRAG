14c84ea3f9d0123d2b661bdc792b22abfc1b7d21
mwalker-tmd <mwalker@tamarkdesigns.com>
2025-04-13 19:52:49 -0600

    Added code so the backend can serve up the React app from the '/' endpoint.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from app.api import endpoints
 
 app = FastAPI()
@@ -20,3 +21,7 @@ app.add_middleware(
 
 # Include the endpoints
 app.include_router(endpoints.router)
+
+# Serve React build from / (root)
+frontend_path = os.path.join(os.path.dirname(__file__), "static")
+app.mount("/", StaticFiles(directory=frontend_path, html=True), name="static")
